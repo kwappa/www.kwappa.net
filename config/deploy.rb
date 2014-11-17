8ed24aab820f53d9a7210b67da992c1f3d6dd607
SHIOYA, Hiromu <kwappa.856@gmail.com>
2014-11-17 11:32:17 +0900

    add task to create symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,10 @@ lock '3.2.1'
 
 set :application, 'www.kwappa.net'
 set :repo_url, 'git@github.com:kwappa/www.kwappa.net.git'
+
+desc 'creat symlink of blog directory into current/'
+task :link_blog_dir do
+  on roles(:web) do
+    execute "cd #{deploy_to} ; ln -s #{deploy_to}/blog current"
+  end
+end
